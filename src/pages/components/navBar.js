8da2b14571878f67dc4f58d80331ba6e85564d43
Bash2009 <athekamebash@gmail>
2025-01-23 20:26:54 +0000

    Edited the Art chronicles path in navBar.js

--- src/pages/components/navBar.js
+++ src/pages/components/navBar.js
@@ -5,7 +5,7 @@ const NavBar = (props) => {
   return (
     <React.Fragment>
       <nav className="navbar navbar-expand-md px-3">
-        <Link to="/" className="navbar-brand fw-bold" >
+        <Link to="/Art-chronicles/" className="navbar-brand fw-bold">
           Art Chronicles
         </Link>
         <button
@@ -24,24 +24,37 @@ const NavBar = (props) => {
             </Link>
 
             <div className="d-none d-md-block dropdown">
-            <Link className="nav-link dropdown-toggle" data-bs-toggle='dropdown' data-bs-target='#nav-dropdown'>Gallery</Link>
+              <Link
+                className="nav-link dropdown-toggle"
+                data-bs-toggle="dropdown"
+                data-bs-target="#nav-dropdown"
+              >
+                Gallery
+              </Link>
 
-            <ul className="dropdown-menu" id="nav-dropdown">
-              <li><Link to='/modernArt' className="dropdown-item" >Modern Arts</Link></li>
-              <li><Link to='/renaissanceArt' className="dropdown-item" >Renaissance Arts</Link></li>
-            </ul>
+              <ul className="dropdown-menu" id="nav-dropdown">
+                <li>
+                  <Link to="/modernArt" className="dropdown-item">
+                    Modern Arts
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/renaissanceArt" className="dropdown-item">
+                    Renaissance Arts
+                  </Link>
+                </li>
+              </ul>
             </div>
 
-            
-            <Link to="/modernArt" className="nav-link d-block d-md-none" >
+            <Link to="/modernArt" className="nav-link d-block d-md-none">
               Modern Art
             </Link>
 
-            <Link to="/renaissanceArt" className="nav-link d-block d-md-none" >
+            <Link to="/renaissanceArt" className="nav-link d-block d-md-none">
               Renaissance Art
             </Link>
 
-            <a href="#contactUs" className="nav-link" >
+            <a href="#contactUs" className="nav-link">
               Contacts
             </a>
           </div>
