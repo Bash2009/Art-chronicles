c84b443b51cdd010d761fa968de515d1d4d63d49
Bash2009 <athekamebash@gmail>
2025-01-23 18:00:13 +0000

    Added react router to index.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import "./App.css";
 import React from "react";
 import FirstPage from "./pages/firstPage";
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
-import ModernArts from "./pages/modernArts";
-import RenaissanceArt from "./pages/renaissanceArt";
-import ErrorPage from "./pages/components/errorElement";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <FirstPage />,
-    errorElement: <ErrorPage />
-  },
-
-  {
-    path: "/modernArt",
-    element: <ModernArts />,
-  },
-
-  {
-    path: "/renaissanceArt",
-    element: <RenaissanceArt />,
-  },
-]);
 
 function App() {
   return (
     <React.Fragment>
-      <RouterProvider router={router} />
+      <FirstPage />
     </React.Fragment>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,14 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import ModernArts from "./pages/modernArts";
+import RenaissanceArt from "./pages/renaissanceArt";
+import ErrorPage from "./pages/components/errorElement";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+
+  {
+    path: "/modernArt",
+    element: <ModernArts />,
+  },
+
+  {
+    path: "/renaissanceArt",
+    element: <RenaissanceArt />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
