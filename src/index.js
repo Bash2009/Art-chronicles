b80d15b90fe2aedd5874d1791e6fcbb6561c94a6
Bash2009 <athekamebash@gmail>
2025-01-23 19:17:51 +0000

    Added base tag to index.html

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
-    path: "/Art-chronicles/",
+    path: "/",
     element: <App />,
     errorElement: <ErrorPage />,
   },
